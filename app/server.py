5c9bc1b9464eb592ec015aa54182d44420497ed4
zalihat mohammed <zalihatmohammad25@gmail.com>
2019-10-22 19:33:39 +0100

    prediction mapping

--- app/server.py
+++ app/server.py
@@ -61,5 +61,6 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    pred_dict = {"0": "Normal", "1": "Tuberculosis manifestation"}
+    return JSONResponse({'result': pred_dict[str(prediction)]})
 
